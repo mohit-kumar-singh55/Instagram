07f2d6dc4a7332e92a7104a716a070bbf5a7a05c
Mohit Kumar Singh <mohitkumarsingh12344@gmail.com>
2022-03-12 18:39:42 +0530

    added faker but of faker@5.5.3 as it is ended!

--- components/Stories.js
+++ components/Stories.js
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import faker from "faker";
 
 const Stories = () => {
+    useEffect(() => {
+        // generating fake data using faker.js
+        const suggestions = [...Array(20)].map((_, i) => ({
+            ...faker.helpers.contextualCard(),
+            i
+        }))
+        console.log(suggestions);
+    }, [])
+
     return (
-        <div>Stories</div>
+        <div>
+            Stories
+        </div>
     )
 }
 
