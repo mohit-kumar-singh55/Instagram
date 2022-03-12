bd7fbe3ad3a5f364390715f4f17f201abfc471ec
Mohit Kumar Singh <mohitkumarsingh12344@gmail.com>
2022-03-12 19:33:11 +0530

    story component completed

--- components/Story.js
+++ components/Story.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Story = ({ profile }) => {
     return (
-        <div>
-            <img src={profile.avatar} alt="user" />
-            <p>{profile.username}</p>
+        <div className='hover:scale-110 transition-all duration-300 cursor-pointer'>
+            <img className='h-14 w-14 rounded-full p-[1.5px] border-red-500 border-2 object-contain' src={profile.avatar} alt="user" />
+            <p className='text-xs w-14 truncate text-center'>{profile.username}</p>
         </div>
     )
 }
