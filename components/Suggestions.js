66d4e2af134f253d6aa229f176d8137bcb6092be
Mohit Kumar Singh <mohitkumarsingh12344@gmail.com>
2022-03-13 20:17:48 +0530

    generating fake data for suggesitons

--- components/Suggestions.js
+++ components/Suggestions.js
@@ -1,6 +1,18 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import faker from "@faker-js/faker";
 
 const Suggestions = () => {
+    const [suggestions, setSuggetions] = useState([]);
+
+    useEffect(() => {
+        const suggetions = [...Array(5)].map((_, i) => (
+            {
+                ...faker.helper.contextualCard(),
+                id: i
+            }
+        ))
+    }, [])
+
     return (
         <div>Suggestions</div>
     )
