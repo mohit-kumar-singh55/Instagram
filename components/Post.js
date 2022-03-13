cb8312ba2f53c5a715f116434795be198990b008
Mohit Kumar Singh <mohitkumarsingh12344@gmail.com>
2022-03-13 18:44:47 +0530

    created dummy data

---
+++ components/Post.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+
+const Post = ({ post }) => {
+    return (
+        <div>
+            {/* Header */}
+
+
+            {/* Img */}
+
+            {/* Buttons */}
+
+            {/* Caption */}
+
+            {/* Comments */}
+
+            {/* Input */}
+        </div>
+    )
+}
+
+export default Post
