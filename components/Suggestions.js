b27e7a69eabc784032f3b06d5aac6dcaa144c8a5
Mohit Kumar Singh <mohitkumarsingh12344@gmail.com>
2022-03-13 20:39:30 +0530

    added suggestions

--- components/Suggestions.js
+++ components/Suggestions.js
@@ -5,16 +5,34 @@ const Suggestions = () => {
     const [suggestions, setSuggetions] = useState([]);
 
     useEffect(() => {
-        const suggetions = [...Array(5)].map((_, i) => (
+        const suggestions = [...Array(5)].map((_, i) => (
             {
-                ...faker.helper.contextualCard(),
+                ...faker.helpers.contextualCard(),
                 id: i
             }
         ))
+
+        setSuggetions(suggestions);
     }, [])
 
     return (
-        <div>Suggestions</div>
+        <div className='mt-4 ml-10'>
+            <div className='flex justify-between text-sm mb-5'>
+                <h3 className='text-sm font-bold text-blue-400'>Suggestions for you</h3>
+                <button className='text-gray-600 font-semibold'>See All</button>
+            </div>
+
+            {suggestions.map((profile) => (
+                <div key={profile.id} className="flex justify-between items-center mt-3">
+                    <img className="w-10 h-10 rounded-full border p-[2px]" src={profile.avatar} alt="suggest" />
+                    <div className="flex-1 ml-4">
+                        <h2 className="font-semibold text-sm ">{profile.username}</h2>
+                        <h3 className="text-xs text-gray-400">{profile.company.name}</h3>
+                    </div>
+                    <button className="text-blue-400 text-sm font-semibold">Follow</button>
+                </div>
+            ))}
+        </div>
     )
 }
 
