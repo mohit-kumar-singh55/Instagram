1bbfe13fd8fe6f9da6d29fe6b11026cacea08c01
Mohit Kumar Singh <mohitkumarsingh12344@gmail.com>
2022-03-13 19:02:29 +0530

    handy fake data

--- components/Post.js
+++ components/Post.js
@@ -1,11 +1,16 @@
 import React from 'react';
-
+import { BookmarkIcon, ChatIcon, DotsHorizontalIcon, EmojiHappyIcon, HeartIcon, PaperAirplaneIcon } from "@heroicons/react/outline";
+import { HeartIconFilled } from "@heroicons/react/solid";
 
 const Post = ({ post }) => {
     return (
         <div>
             {/* Header */}
-
+            <div>
+                <img src={post.userImg} alt="user" />
+                <p>{post.username}</p>
+                <DotsHorizontalIcon />
+            </div>
 
             {/* Img */}
 
--- components/Posts.js
+++ components/Posts.js
@@ -1,9 +1,39 @@
 import React from 'react';
+import Post from "./Post";
+import faker from '@faker-js/faker';
 
-const Post = () => {
+const posts = [
+    {
+        id: Date.now(),
+        username: "sniper.mks_55",
+        userImg: faker.helpers.contextualCard().avatar,
+        img: faker.helpers.contextualCard().avatar,
+        caption: "This is a post you know!"
+    },
+    {
+        id: Date.now(),
+        username: "mks_55",
+        userImg: faker.helpers.contextualCard().avatar,
+        img: faker.helpers.contextualCard().avatar,
+        caption: "This is a post you know!"
+    },
+    {
+        id: Date.now(),
+        username: "sniper_55",
+        userImg: faker.helpers.contextualCard().avatar,
+        img: faker.helpers.contextualCard().avatar,
+        caption: "This is a post you know!"
+    },
+]
+
+const Posts = () => {
     return (
-        <div>Post</div>
+        <div>
+            {posts.map((post) => (
+                <Post post={post} key={post.id} />
+            ))}
+        </div>
     )
 }
 
-export default Post;
+export default Posts;
