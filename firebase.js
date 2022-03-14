06eedf932cc3c769fe692f0bc40cd5c276044e13
Mohit Kumar Singh <mohitkumarsingh12344@gmail.com>
2022-03-14 19:40:57 +0530

    firebase initialized

--- firebase.js
+++ firebase.js
@@ -1,8 +1,14 @@
-import { initializeApp } from 'firebase/app';
+import { initializeApp, getApps, getApp } from 'firebase/app';
 
 
 const firebaseConfig = {
-
+    apiKey: "AIzaSyB3-9hD9qxy1C8tDIZA3F_cf4KLKSni7aA",
+    authDomain: "insta-for-me-a9903.firebaseapp.com",
+    projectId: "insta-for-me-a9903",
+    storageBucket: "insta-for-me-a9903.appspot.com",
+    messagingSenderId: "168826167230",
+    appId: "1:168826167230:web:1808b73acd96c3599de3ef"
 };
 
-const app = initializeApp(firebaseConfig);
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
