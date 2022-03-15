706d782c40fe3040f32eee5e5defd6b241a91eef
Mohit Kumar Singh <mohitkumarsingh12344@gmail.com>
2022-03-15 20:24:05 +0530

    fetched providers

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,13 +1,28 @@
 import React from 'react';
+import { getProviders, signIn } from "next-auth/react";
 
-const SignIn = () => {
+const SignIn = ({ providers }) => {
     return (
-        <div>signin</div>
+        <>
+            {Object.values(providers).map((provider) => (
+                <div key={provider?.name}>
+                    <button onClick={() => signIn(provider.id)}>
+                        Sign in with {provider.name}
+                    </button>
+                </div>
+            ))}
+        </>
     )
 }
 
 export default SignIn;
 
-export async function getServerSideProps(){
+export async function getServerSideProps() {
+    const providers = await getProviders();
 
+    return {
+        props: {
+            providers,
+        }
+    }
 }
