f83a8a61eed55dca06f1244b915ac20193656a62
Mohit Kumar Singh <mohitkumarsingh12344@gmail.com>
2022-03-15 19:00:04 +0530

    making signin page

---
+++ pages/auth/signin.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const SignIn = () => {
+    return (
+        <div>signin</div>
+    )
+}
+
+export default SignIn;
+
+export async function getServerSideProps(){
+
+}
