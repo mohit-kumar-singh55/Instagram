75cbd50fd74ee27bef128104372b450d0559af9f
Mohit Kumar Singh <mohitkumarsingh12344@gmail.com>
2022-03-16 20:56:31 +0530

    Maked own story dynamic

--- components/Stories.js
+++ components/Stories.js
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import faker from "@faker-js/faker";
 import Story from "./Story";
+import { useSession } from 'next-auth/react';
 
 const Stories = () => {
+    const { data: session } = useSession();
+
     const [suggestions, setSuggestions] = useState([]);
 
     useEffect(() => {
@@ -16,8 +19,12 @@ const Stories = () => {
 
     return (
         <div className='flex space-x-2 p-6 bg-white mt-8 border-gray-200 border rounded-sm overflow-x-scroll'>
+            {session && (
+                <Story username={session?.user?.username} image={session?.user?.image} />
+            )}
+
             {suggestions.map((profile) => (
-                <Story key={profile.id} profile={profile} />
+                <Story key={profile.id} username={profile.username} image={profile.avatar} />
             ))}
         </div>
     )
--- components/Story.js
+++ components/Story.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const Story = ({ profile }) => {
+const Story = ({ username, image }) => {
     return (
         <div className='hover:scale-110 transition-all duration-300 cursor-pointer'>
-            <img className='h-14 w-14 rounded-full p-[1.5px] border-red-500 border-2 object-contain' src={profile.avatar} alt="user" />
-            <p className='text-xs w-14 truncate text-center'>{profile.username}</p>
+            <img className='h-14 w-14 rounded-full p-[1.5px] border-red-500 border-2 object-contain' src={image} alt="user" />
+            <p className='text-xs w-14 truncate text-center'>{username}</p>
         </div>
     )
 }
