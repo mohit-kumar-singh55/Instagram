3aa556735b1e91e3cc52c25003153f1d580a01fd
Mohit Kumar Singh <mohitkumarsingh12344@gmail.com>
2022-03-16 20:50:35 +0530

    features for signOut user

--- components/MiniProfile.js
+++ components/MiniProfile.js
@@ -7,16 +7,12 @@ const MiniProfile = () => {
 
     return (
         <div className='flex items-center justify-between mt-14 ml-10'>
-            {session && (
-                <>
-                    <img className='w-16 h-16 rounded-full border p-[2px]' src={session?.user?.image} alt="user" />
-                    <div className='flex-1 mx-4'>
-                        <h2 className='font-bold'>{session?.user?.username}</h2>
-                        <h3 className='text-sm text-gray-500'>Welcome to Instagram</h3>
-                    </div>
-                    <button className='text-blue-400 text-sm font-semibold' onClick={signOut}>Sign Out</button>
-                </>
-            )}
+            <img className='w-16 h-16 rounded-full border p-[2px]' src={session?.user?.image} alt="user" />
+            <div className='flex-1 mx-4'>
+                <h2 className='font-bold'>{session?.user?.username}</h2>
+                <h3 className='text-sm text-gray-500'>Welcome to Instagram</h3>
+            </div>
+            <button className='text-blue-400 text-sm font-semibold' onClick={signOut}>Sign Out</button>
         </div>
     )
 }
