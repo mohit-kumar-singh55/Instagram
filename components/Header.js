45389bd2e5a29e0ba2c181aedcd91a72f30b6721
Mohit Kumar Singh <mohitkumarsingh12344@gmail.com>
2022-03-16 18:39:49 +0530

    maked sign and out functionality

--- components/Header.js
+++ components/Header.js
@@ -2,8 +2,11 @@ import React from 'react';
 import Image from "next/image";
 import { SearchIcon, PlusCircleIcon, UserGroupIcon, HeartIcon, PaperAirplaneIcon, MenuIcon } from "@heroicons/react/outline";
 import { HomeIcon } from "@heroicons/react/solid";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 const Header = () => {
+    const { data: session } = useSession();
+
     return (
         <nav className='shadow-md backdrop-blur-sm sticky top-0 z-50'>
             <div className='flex justify-between max-w-6xl mx-5 lg:mx-auto'>
@@ -30,15 +33,21 @@ const Header = () => {
                     <HomeIcon className='navbtn' />
                     <MenuIcon className='h-6 md:hidden' />
 
-                    <div className='relative navbtn'>
-                        <PaperAirplaneIcon className='navbtn rotate-45' />
-                        <div className="absolute -top-1 -right-2 text-sm w-5 h-5 bg-red-500 rounded-full flex items-center justify-center animate-pulse text-white">3</div>
-                    </div>
-                    <PlusCircleIcon className='navbtn' />
-                    <UserGroupIcon className='navbtn' />
-                    <HeartIcon className='navbtn' />
+                    {session ? (
+                        <>
+                            <div className='relative navbtn'>
+                                <PaperAirplaneIcon className='navbtn rotate-45' />
+                                <div className="absolute -top-1 -right-2 text-sm w-5 h-5 bg-red-500 rounded-full flex items-center justify-center animate-pulse text-white">3</div>
+                            </div>
+                            <PlusCircleIcon className='navbtn' />
+                            <UserGroupIcon className='navbtn' />
+                            <HeartIcon className='navbtn' />
 
-                    <img src="https://links.papareact.com/3ke" alt="user_pic" className='h-10 rounded-full cursor-pointer' />
+                            <img src={session?.user?.image} onClick={signOut()} alt="user_pic" className='h-10 rounded-full cursor-pointer' />
+                        </>
+                    ) : (
+                        <button onClick={signIn()}>Sign In</button>
+                    )}
                 </div>
             </div>
         </nav>
